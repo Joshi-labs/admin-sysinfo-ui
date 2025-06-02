0eec737db7466836c99be785d060479fb19c0703
Vishwash Joshi <vishwashmax@gmail.com>
2025-06-02 14:50:47 +0530

    fixes

---
+++ dist/js/pages/dashboard.js
@@ -0,0 +1,163 @@
+
+$(function () {
+  'use strict'
+
+  /* jQueryKnob */
+  $('.knob').knob()
+
+  // The Calender
+  $('#calendar').datetimepicker({
+    format: 'L',
+    inline: true
+  })
+
+
+  /* Chart.js Charts */
+  // Temp chart
+  var cpuTempChartCanvas = document.getElementById('cpu-temp-chart-canvas').getContext('2d')
+
+  var cpuTempChartData = {
+    labels: ['6', '7', '8', '9', '10', '11', '12', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
+    datasets: [
+      {
+        label: 'CPU Temperature',
+        backgroundColor: 'rgba(210, 214, 222, 1)',
+        borderColor: 'rgba(210, 214, 222, 1)',
+        pointRadius: false,
+        pointColor: 'rgba(210, 214, 222, 1)',
+        pointStrokeColor: '#c1c7d1',
+        pointHighlightFill: '#fff',
+        pointHighlightStroke: 'rgba(220,220,220,1)',
+        data: [65, 59, 80, 81, 56, 55, 40]
+      }
+    ]
+  }
+
+  var cpuTempChartOptions = {
+    maintainAspectRatio: false,
+    responsive: true,
+    legend: {
+      display: false
+    },
+    scales: {
+      xAxes: [{
+        gridLines: {
+          display: true
+        }
+      }],
+      yAxes: [{
+        gridLines: {
+          display: true
+        }
+      }]
+    }
+  }
+
+  // This will get the first returned node in the jQuery collection.
+  // eslint-disable-next-line no-unused-vars
+  var cpuTempChart = new Chart(cpuTempChartCanvas, { // lgtm[js/unused-local-variable]
+    type: 'line',
+    data: cpuTempChartData,
+    options: cpuTempChartOptions
+  })
+
+  // CPU util chart
+  var cpuUtilChartCanvas = $('#line-chart').get(0).getContext('2d')
+
+  var cpuUtilChartData = {
+    labels: ['6', '7', '8', '9', '10', '11', '12', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
+    datasets: [
+      {
+        label: 'Digital Goods',
+        fill: false,
+        borderWidth: 2,
+        lineTension: 0,
+        spanGaps: true,
+        borderColor: '#efefef',
+        pointRadius: 3,
+        pointHoverRadius: 7,
+        pointColor: '#efefef',
+        pointBackgroundColor: '#efefef',
+        data: [55, 42, 44, 43, 43, 44, 42, 42, 43, 30]
+      }
+    ]
+  }
+
+  var cpuUtilChartOptions = {
+    maintainAspectRatio: false,
+    responsive: true,
+    legend: {
+      display: false
+    },
+    scales: {
+      xAxes: [{
+        ticks: {
+          fontColor: '#efefef'
+        },
+        gridLines: {
+          display: false,
+          color: '#efefef',
+          drawBorder: false
+        }
+      }],
+      yAxes: [{
+        ticks: {
+          stepSize: 5000,
+          fontColor: '#efefef'
+        },
+        gridLines: {
+          display: true,
+          color: '#efefef',
+          drawBorder: false
+        }
+      }]
+    }
+  }
+
+  // This will get the first returned node in the jQuery collection.
+  // eslint-disable-next-line no-unused-vars
+  var cpuUtilChart = new Chart(cpuUtilChartCanvas, {
+    type: 'line',
+    data: cpuUtilChartData,
+    options: cpuUtilChartOptions
+  })
+})
+
+
+//-------------
+  // - PIE CHART -
+  //-------------
+  // Get context with jQuery - using jQuery's .get() method.
+  var pieChartCanvas = $('#pieChart').get(0).getContext('2d')
+  var pieData = {
+    labels: [
+      'Project A',
+      'Project B',
+      'Project C',
+      'Project D',
+      'Project E',
+    ],
+    datasets: [
+      {
+        data: [1, 2, 1, 4, 2],
+        backgroundColor: ['#f56954', '#00a65a', '#f39c12', '#00c0ef', '#3c8dbc']
+      }
+    ]
+  }
+  var pieOptions = {
+    legend: {
+      display: false
+    }
+  }
+  // Create pie or douhnut chart
+  // You can switch between pie and douhnut using the method below.
+  // eslint-disable-next-line no-unused-vars
+  var pieChart = new Chart(pieChartCanvas, {
+    type: 'doughnut',
+    data: pieData,
+    options: pieOptions
+  })
+
+  //-----------------
+  // - END PIE CHART -
+  //-----------------
